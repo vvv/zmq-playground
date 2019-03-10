872d1c99697f1154d10a04541ee18e81deab6e48
Valery V. Vorotyntsev <valery.vv@gmail.com>
2019-03-10 23:17:08 +0200

    zmq-hello: Add server code
    See http://zguide.zeromq.org/hs:hwserver

--- zmq-hello/app/Main.hs
+++ zmq-hello/app/Main.hs
@@ -1,6 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import           Control.Concurrent (threadDelay)
+import           Control.Monad (forever)
+import           Control.Monad.IO.Class (liftIO)
+import qualified Data.ByteString.Char8 as BS
+import           System.ZMQ4.Monadic
+  ( Rep(..)
+  , bind
+  , receive
+  , runZMQ
+  , send
+  , socket
+  )
 
 main :: IO ()
-main = someFunc
+main = runZMQ $ do
+    -- Socket to talk to clients
+    responder <- socket Rep
+    bind responder "tcp://*:5555"
+
+    forever $ do
+        buffer <- receive responder
+        liftIO $ do
+            putStrLn "Received Hello"
+            BS.putStrLn buffer
+            threadDelay 1000000 -- Do some 'work'
+        send responder [] "World"
